33d197781fc6790e23e30759910cd0e4c50f992e
Jacqueline Zhang <jgzhang@berkeley.edu>
2018-11-03 16:51:54 -0700

    Algorithm progress maybe :')

--- StyleMe.py
+++ StyleMe.py
@@ -1,5 +1,12 @@
 from pprint import pprint
 import requests
+from visual_recognition_v3 import visRec
+
+# Categories for the articles of clothing
+tops = ["tshirts", "tanktops", "blouses", "polos", "sweaters", "longsleeves"]
+outerwear = ["hoodies", "denimjacket", "bomberjackets", "regularjackets"]
+bottoms = ["skirts", "denimshorts", "shorts", "regularpants", "jeans"]
+dresses = ["dresses"]
 
 city = input("What city are you in? ")
 
@@ -9,9 +16,43 @@ jsonData = r.json()
 
 maxKelvin = jsonData['main']['temp_max']
 minKelvin = jsonData['main']['temp_min']
+windMPH = jsonData['wind']['speed']
+
+# pprint(jsonData)
 
 def KelvinToFaren(K):
     return (9/5) * (K - 273) + 32
 
 maxTemp = KelvinToFaren(maxKelvin)
 minTemp = KelvinToFaren(minKelvin)
+
+# Determines which articles of clothing we will restrict
+def restrictOutfits(maxTemp, minTemp, windMPH):
+    clothes = visRec()
+    filteredOptions = []
+    if windMPH > 1:
+        for i in clothes:
+            for j in i:
+                if i.get(j) != "skirts" and i.get(j) != "dresses":
+                    filteredOptions.append(i)
+        clothes = filteredOptions
+        filteredOptions = []
+    if maxTemp > 85:
+        f = lambda x: x[1] == "denimshorts" or x[1] == "shorts" or x[1] == "tshirts" or x[1] == "polos" or x[1] == "skirts" or x[1] == "dresses"
+    elif minTemp > 70:
+        f = lambda x: x[1] != "denimjacket" and x[1] != "bomberjackets" and x[1] != "hoodies" and x[1] != "regularjackets"
+    else:
+        f = lambda x: x[1] != "skirts" and x[1] != "dresses" and x[1] != "shorts" and x[1] != "tanktops" and x[1] != "denimshorts"
+    for c in clothes:
+        filteredOptions.extend(list(filter(f, c.items())))
+    return filteredOptions
+
+def pairOutfits():
+    outfits = restrictOutfits(maxTemp, minTemp, windMPH)
+    print(maxTemp)
+    print(minTemp)
+    print(windMPH)
+    for c in outfits:
+        print(c)
+
+pairOutfits()
--- visual_recognition_v3.py
+++ visual_recognition_v3.py
@@ -4,36 +4,30 @@ from os.path import abspath
 from watson_developer_cloud import VisualRecognitionV3, WatsonApiException
 import os
 
-
-clothes = []
-
 visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     url='https://gateway.watsonplatform.net/visual-recognition/api',
     iam_apikey='_rTi9ExzLh2F_cNt6NRksfQz_sAJ7NhdlXchff5poiF0')
 
-filelist = os.listdir("assets/images/")
-for i in filelist:
-    if i.endswith(".jpg") or i.endswith(".jpeg"):
-        print("...")
-        with open("assets/images/" + i, 'rb') as images_file:
-            classes = visual_recognition.classify(
-                #[cv2.imread(img) for img in glob.glob("assets/images/*.jpg")],
-                images_file,
-                threshold='0.5',
-                classifier_ids='DefaultCustomModel_2095219532').get_result()
-            #print(json.dumps(classes, indent=2))
-            thisItem = classes['images'][0].get('classifiers')[0].get('classes')
-            if len(thisItem) > 1:
-                macks = 0
-                thing = {}
-                for d in thisItem:
-                    macks = max(macks, d['score'])
-                    if macks == d['score']:
-                        thing = d
-                thisItem = [thing]
-            clothes.append(thisItem[0])
-            #clothes.append([])
-
-for c in clothes:
-    print(c)
+def visRec():
+    clothes = []
+    filelist = os.listdir("assets/images/")
+    for i in filelist:
+        if i.endswith(".jpg") or i.endswith(".jpeg"):
+            with open("assets/images/" + i, 'rb') as images_file:
+                classes = visual_recognition.classify(
+                    images_file,
+                    threshold='0.5',
+                    classifier_ids='DefaultCustomModel_2095219532').get_result()
+                thisItem = classes['images'][0].get('classifiers')[0].get('classes')
+                if len(thisItem) > 1:
+                    tempMax = 0
+                    thing = {}
+                    for d in thisItem:
+                        tempMax = max(tempMax, d['score'])
+                        if tempMax == d['score']:
+                            tempMaxItem = d
+                    thisItem = [tempMaxItem]
+                thisItem = thisItem[0].get('class')
+                clothes.append({"assets/images/" + i: thisItem})
+    return clothes
