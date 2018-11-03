ee7ada17b046ae86899d7eb62a97e8bd210118aa
Sahil Sanghvi <sanghviss@berkeley.edu>
2018-11-03 12:17:49 -0700

    Temps now convert to Farenheit

--- WeatherServices.py
+++ WeatherServices.py
@@ -2,9 +2,16 @@ from pprint import pprint
 import requests
 
 city = input("What city are you in? ")
-#print('CITY: ' + city )
+
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=5aaf03aa7fa904874c08c91d456f6d7b'
-#pprint('URL is:   ' + url)
 r = requests.get(url)
-#pprint(r.json())
 jsonData = r.json()
+
+maxKelvin = jsonData['main']['temp_max']
+minKelvin = jsonData['main']['temp_min']
+
+def KelvinToFaren(K):
+    return (9/5) * (K - 273) + 32
+
+maxTemp = KelvinToFaren(maxKelvin)
+minTemp = KelvinToFaren(minKelvin)
